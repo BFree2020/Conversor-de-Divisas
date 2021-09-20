d5d4cbfc6fb433354ef3afa03ab3b575519e9ecf
“Ivanc” <ivancast130@gmail.com>
2021-09-20 11:38:11 -0500

    Primera actualizacion

---
+++ script.js
@@ -0,0 +1,115 @@
+function capturarDatos() {
+
+    let cantidaddedinero = 0,
+
+    cantidaddedinero = prompt("ingrese cantidad de dinero");
+
+    alert("Total es" + cantidaddedinero);
+
+ public class main{
+
+    public static void{
+        while (true){
+            system.out.println("elige tu moneda");
+            system.out.println("1 - Dolar a peso colombiano\n"
+                    +"2 - Dolar a Euro\n"                                           
+                    +"3 - Dolar a Libra esterlina\n"
+                    +"4 - Peso Colombiano a Dolar\n"
+                    +"5 - Peso Colombiano a Euro\n"
+                    +"6 - Peso Colombiano a Libra esterlina\n"
+                    +"7 - Euro a Dolar\n"
+                    +"8 - Euro a Peso Colombiano\n"
+                    +"9 - Euro a Libra Esterlina\n"
+                    +"10 - Libra Esterlina a Dolar\n"
+                    +"11 - Libra Esterlina a Peso Colombiano\n" 
+                    +"12 - Libra Esterlina a Euro\n"
+                    +"13 - Salir");
+            system.out.println("moneda");
+            scanner leer = new scanner(system.in);
+            chart opcion = leer.next().charAt(0);
+
+            switch (opcion)
+        }
+    }
+    static void convertir(double valorDolar, string pais){
+    scanner leer = new scanner(system.in);
+    system.out.printf("ingrese la cantidad de %s :" , pais);
+    double cantidaMoneda = leer.nextDouble();
+
+    Double Dolares = cantidadMoneda / valorDolar;
+
+    public class main{
+        public static void main(string[] args) {
+            while (true){
+    }           system.out.println("elige tu moneda");
+    system.out.println("1 - Dolar a Peso Colombiano\n"
+            +"2 - Dolar a Euro\n"                                           
+            +"3 - Dolar a Libra esterlina\n"
+            +"4 - Peso Colombiano a Dolar\n"
+            +"5 - Peso Colombiano a Euro\n"
+            +"6 - Peso Colombiano a Libra esterlina\n"
+            +"7 - Euro a Dolar\n"
+            +"8 - Euro a Peso Colombiano\n"
+            +"9 - Euro a Libra Esterlina\n"
+            +"10 - Libra Esterlina a Dolar\n"
+            +"11 - Libra Esterlina a Peso Colombiano\n" 
+            +"12 - Libra Esterlina a Euro\n"
+            +"13 - Salir");
+    system.out.println("moneda");
+    scanner leer = new scanner(system.in);
+    chart opcion = leer.next().charAt(0);
+
+    switch (opcion){
+        case '1':
+            convertir(3813, "Peso Colombiano");
+            break;
+        case '2':
+            convertir(0.84888, "Euro");
+            break;
+        case '3':
+            convertir(0.72403, "Libra esterlina");
+            break;
+    
+        case '4':
+            convertir(0.00026, "Dolar");
+            break;
+        case '5':
+            convertir(0.00022, "Euro");
+            break;
+        case '6':
+            convertir(0.00019, "Libra esterlina");
+            break;
+
+        case '7':
+            convertir(1.17787, "Dolar");
+            break;
+        case '8':
+            convertir(4492.11, "Euro");
+            break;
+        case '9':
+            convertir(0.85284, "Libra esterlina");
+            break;
+
+        case '10':
+            convertir(1.38097, "Dolar");
+            break;
+        case '11':
+            convertir(5266.67, "Euro");
+            break;
+        case '12':
+            convertir(1.1723, "Libra esterlina");
+            break;
+
+        case '13':
+            system.out.println("cerrando programa");
+            break;
+        }    
+    }
+
+
+   
+}
+    }
+console.log(cantidaddedinero,eligetumoneda)
+
+
